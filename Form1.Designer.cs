884d29a07011814ebaad1ac964904a3b2e7471a1
Tonic-Box <73169574+Tonic-Box@users.noreply.github.com>
2020-12-03 09:05:10 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace GSecPs3Decrypter
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(513, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Location = new System.Drawing.Point(452, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 20);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.Info;
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 60);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(573, 122);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InfoText;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.Location = new System.Drawing.Point(93, 10);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(49, 15);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "Status:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::GSecPs3Decrypter.Properties.Resources.icon;
+            this.pictureBox1.Location = new System.Drawing.Point(531, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.InfoText;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.7F);
+            this.textBox3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.textBox3.Location = new System.Drawing.Point(143, 10);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(303, 15);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "Idle...";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(597, 192);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "[GSec] Ps3 Decrypter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void TextBox1_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using PS3FileSystem;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace GSecPs3Decrypter
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			if (this.fbd.ShowDialog() == DialogResult.OK)
+			{
+				this.button2.Enabled = false;
+				this.listView1.Items.Clear();
+				this.dirpath = this.fbd.SelectedPath;
+				this.textBox1.Text = this.fbd.SelectedPath;
+				this.button2.Visible = true;
+				this.button2.Enabled = true;
+				string state;
+				if (File.Exists(this.dirpath + "\\\\~decrypted.txt"))
+				{
+					this.button2.Text = "Encrypt";
+					state = "✓";
+					this.pictureBox1.Image = global::GSecPs3Decrypter.Properties.Resources.unlock;
+				}
+				else
+				{
+					this.button2.Text = "Decrypt";
+					state = "🔒";
+					this.pictureBox1.Image = global::GSecPs3Decrypter.Properties.Resources._lock;
+				}
+				try
+				{
+					Ps3SaveManager ps3SaveManager = new Ps3SaveManager(this.textBox1.Text, this.key);
+					for (int i = 0; i <= ps3SaveManager.Param_PFD.Entries.Length - 1; i++)
+					{
+						this.listView1.Items.Add("[" + state + "] " + ps3SaveManager.Param_PFD.Entries[i].file_name);
+					}
+					this.SetStatus("Loaded directory");
+				}
+				catch(Exception z)
+				{
+					this.listView1.Items.Clear();
+					this.button2.Visible = false;
+					this.button2.Enabled = false;
+					this.SetStatus(z.Message);
+					this.pictureBox1.Image = global::GSecPs3Decrypter.Properties.Resources.icon;
+				}
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if(this.button2.Text == "Encrypt")
+			{
+				this.listView1.Items.Clear();
+				Ps3SaveManager ps3SaveManager = new Ps3SaveManager(this.dirpath, this.key);
+				this.SetStatus(ps3SaveManager.EncryptAllFiles().ToString() + " Files Encrypted");
+				File.Delete(this.dirpath + "\\\\~decrypted.txt");
+				for (int i = 0; i <= ps3SaveManager.Param_PFD.Entries.Length - 1; i++)
+				{
+					this.listView1.Items.Add("[🔒] " + ps3SaveManager.Param_PFD.Entries[i].file_name);
+				}
+				this.button2.Text = "Decrypt";
+				this.pictureBox1.Image = global::GSecPs3Decrypter.Properties.Resources._lock;
+			}
+			else if(this.button2.Text == "Decrypt")
+			{
+				this.listView1.Items.Clear();
+				Ps3SaveManager ps3SaveManager = new Ps3SaveManager(this.dirpath, this.key);
+				this.SetStatus(ps3SaveManager.DecryptAllFiles().ToString() + " Files Decrypted");
+				string text = "";
+				for (int i = 0; i <= ps3SaveManager.Param_PFD.Entries.Length - 1; i++)
+				{
+					text = text + " " + ps3SaveManager.Param_PFD.Entries[i].file_name;
+					this.listView1.Items.Add("[✓] " + ps3SaveManager.Param_PFD.Entries[i].file_name);
+				}
+				File.WriteAllText(this.dirpath + "\\\\~decrypted.txt", text);
+				this.button2.Text = "Encrypt";
+				this.pictureBox1.Image = global::GSecPs3Decrypter.Properties.Resources.unlock;
+			}
+		}
+
+		public void SetStatus(string text)
+		{
+			this.textBox3.Text = text;
+		}
+
+		public FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+		// Token: 0x0400003F RID: 63
+		private string dirpath;
+
+		// Token: 0x04000040 RID: 64
+		public byte[] key = new byte[]
+		{
+			202,
+			239,
+			174,
+			185,
+			39,
+			34,
+			167,
+			203,
+			83,
+			93,
+			82,
+			135,
+			34,
+			250,
+			104,
+			221
+		};
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			this.textBox1.KeyDown += textBox1_KeyDown;
+		}
+
+		private void textBox1_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter)
+			{
+				if (Directory.Exists(this.textBox1.Text))
+				{
+					this.listView1.Items.Clear();
+					this.dirpath = this.textBox1.Text;
+					this.fbd.SelectedPath = this.textBox1.Text;
+
+					this.button2.Visible = true;
+					this.button2.Enabled = true;
+					string state;
+					if (File.Exists(this.dirpath + "\\\\~decrypted.txt"))
+					{
+						this.button2.Text = "Encrypt";
+						state = "✓";
+						this.pictureBox1.Image = global::GSecPs3Decrypter.Properties.Resources.unlock;
+					}
+					else
+					{
+						this.button2.Text = "Decrypt";
+						state = "🔒";
+						this.pictureBox1.Image = global::GSecPs3Decrypter.Properties.Resources._lock;
+					}
+					try
+					{
+						Ps3SaveManager ps3SaveManager = new Ps3SaveManager(this.textBox1.Text, this.key);
+						for (int i = 0; i <= ps3SaveManager.Param_PFD.Entries.Length - 1; i++)
+						{
+							this.listView1.Items.Add("[" + state + "] " + ps3SaveManager.Param_PFD.Entries[i].file_name);
+						}
+						this.SetStatus("Loaded directory");
+					}
+					catch (Exception z)
+					{
+						this.listView1.Items.Clear();
+						this.button2.Visible = false;
+						this.button2.Enabled = false;
+						this.pictureBox1.Image = global::GSecPs3Decrypter.Properties.Resources.icon;
+						this.SetStatus(z.Message);
+					}
+				}
+				else
+				{
+					this.pictureBox1.Image = global::GSecPs3Decrypter.Properties.Resources.icon;
+					this.listView1.Items.Clear();
+					this.SetStatus("Directory does not exist");
+				}
+					
+			}
+		}
+	}
+}
